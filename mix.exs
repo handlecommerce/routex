4144f2418b8e42c4fa78b0a0a551b539afdec0fd
Mark Glenn <markglenn@gmail.com>
2025-01-17 16:45:56 -0600

    Update name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Routex.MixProject do
 
   def project do
     [
-      app: :route_match,
+      app: :routex,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
